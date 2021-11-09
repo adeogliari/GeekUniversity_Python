136e685ff630320e1f2398fb2432c42cd3c0cdee
Ademir Ogliari <ademir.ogliari@saude.gov.br>
2021-11-09 11:44:55 -0300

    Adicionados arquivos das aulas da seção 05 do curso

---
+++ s05_estruturas_logicas_e_condicionais/s05a02_estruturas_logicas_and_or_not_is.py
@@ -0,0 +1,16 @@
+"""
+Estruturas Lógicas: and (e), or (ou), not (não), is (é)
+
+Operadores unários:
+    - not
+Operadores binários:
+    - and, or, is
+"""
+
+ativo = True
+logado = True
+
+if ativo and logado:
+    print('Bem-vindo usuário!')
+else:
+    print('Você precisa ativar sua conta. Por favor, cheque seu e-mail')
