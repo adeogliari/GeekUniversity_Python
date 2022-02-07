e8bff09ff44f74d3a193eb30c33f12ef665fcaa9
Ademir Ogliari <ad.ogliari@hotmail.com>
2022-02-07 17:57:00 -0300

    Iniciada aula 02 da seção 07

--- s07_colecoes/s07a02_tuplas.py
+++ s07_colecoes/s07a02_tuplas.py
@@ -1,8 +1,35 @@
-import unittest
+"""
+Tuplas (tuple)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+Tuplas são bastante parecidas com listas.
 
-if __name__ == '__main__':
-    unittest.main()
+Existem basicamente duas diferenças:
+    1 - As tuplas são representadas por parênteses ()
+    2 - As tuplas são imutáveis: Isso significa que ao se criar uma tupla ela não
+        muda. Toda operação em uma tupla gera uma nova tupla
+
+# CUIDADO 1: As tuplas são representadas por (), mas veja:
+    tupla1 = (1, 2, 3, 4, 5, 6)
+    print(tupla1)
+    print(type(tupla1))
+
+    tupla2 = 1, 2, 3, 4, 5, 6
+    print(tupla2)
+    print(type(tupla2))
+
+# CUIDADO 2: Tuplas com 1 elemento
+    tupla3 = (4) # Isso não é uma tupla!
+    print(tupla3)
+    print(type(tupla3))
+
+    tupla4 = (4,) # Isso é uma tupla!
+    print(tupla4)
+    print(type(tupla4))
+
+# CONCLUSÃO: As tuplas são definidas pela vírgula e não pelo uso do parênteses
+    tupla5 = 5, # Isso é uma tupla!
+    print(tupla5)
+    print(type(tupla5))
+"""
+
+
